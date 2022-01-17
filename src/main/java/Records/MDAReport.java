9c1305c5f4b02dc1ced8f0dd4e9eb921592c13a6
Noabelle Robins <noabelle3@gmail.com>
2022-01-17 09:37:15 +0200

    Added MDAReport class

---
+++ src/main/java/Records/MDAReport.java
@@ -0,0 +1,16 @@
+package Records;
+
+public class MDAReport extends RecordData{
+    String mdaCode;
+    String idNum;
+    String idType;
+    String firstName;
+    String lastName;
+    String city;
+    String street;
+    String buildingNumber;
+    String barcode;
+    String getDate;
+    String takeDate;
+    String resultDate;
+}
